ac8710167ad9d27866ec3529008837aef54d2434
newtonjob <jobnewton3@gmail.com>
2022-11-15 23:07:56 +0100

    Improve the registration flow and clean up the web routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/login',       [AuthController::class, 'login'])->name('login');
+Route::post('/register',    [RegisterController::class, 'store'])->name('register.store');
 
 Route::middleware('auth')->group(function () {
     //
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,11 @@
 <?php
 
-use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\SubscriptionController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\UserPhotoController;
 use App\Http\Controllers\UserSubscriptionController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\LogoutController;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -30,31 +28,15 @@ Route::redirect('/', 'dashboard');
 
 Route::middleware('guest')->group(function () {
     Route::view('/login',       'auth.login')->name('login');
-    Route::get('/register',    [RegisterController::class, 'create'])->name('register.create');
-    Route::post('/register',   [RegisterController::class, 'store'])->name('register.store');
+    Route::view('/register',    'auth.register')->name('register.create');
 });
 
 Route::middleware('auth')->group(function () {
     Route::view('/dashboard', 'users.dashboard')->name('dashboard');
 
-    Route::controller(UserController::class)->group(function () {
-        Route::get('/users', 'index')             ->name('users.index');
-        Route::get('/users/{user}', 'show')       ->name('users.show');
-        Route::post('/users', 'store')            ->name('users.store');
-        Route::put('/users/{user}', 'update')     ->name('users.update');
-        Route::delete('/users/{user}', 'destroy') ->name('users.destroy');
-    });
-
-    Route::controller(SubscriptionController::class)->group(function () {
-        Route::get('/subscriptions', 'index')              ->name('subscriptions.index');
-        Route::get('/subscriptions/{subscription}', 'show')->name('subscriptions.show');
-    });
-
-    Route::controller(UserSubscriptionController::class)->group(function () {
-        Route::get('/users/{user}/subscriptions', 'index') ->name('users.subscriptions.index');
-        Route::post('/users/{user}/subscriptions', 'store')->name('users.subscriptions.store');
-
-    });
+    Route::resource('users',                UserController::class)->except('create');
+    Route::resource('subscriptions',        SubscriptionController::class)->only(['index', 'show']);
+    Route::resource('users.subscriptions',  UserSubscriptionController::class)->only(['index', 'store']);
 
     Route::controller(UserPhotoController::class)->group(function () {
         Route::put('/photos/{user}', 'update')    ->name('photos.update');
