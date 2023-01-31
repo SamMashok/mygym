521138f7e7990e7a51576ae6e3be66747415f1d3
sammashok <sammashtiunz@gmail.com>
2023-01-31 01:15:12 +0100

    Update HomeController.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,16 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Subscription;
-use Illuminate\Support\Facades\Auth;
-
 class HomeController extends Controller
 {
-
     public function show()
     {
-        $monthly_sub = Subscription::where('user_id', Auth::id())->thisMonth()->count();
-        $tot_amount  = Subscription::where('user_id','=', Auth::id())->thisMonth()->successful()->sum('amount');
+        $monthly_sub = user()->subscriptions()->thisMonth()->count();
+        $tot_amount  = user()->subscriptions()->thisMonth()->successful()->sum('amount');
 
         return view('users.dashboard', ['monthly_sub' => $monthly_sub, 'tot_amount' => $tot_amount]);
     }
