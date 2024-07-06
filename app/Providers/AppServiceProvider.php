ff70462c2456047e63253264b216ba9fcba4786c
sammashok <samuelmashok@gmail.com>
2024-07-06 18:23:16 +0100

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,32 +27,27 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->registerResponseMacros();
+        $this->registerApiResponseMacro();
 
-        Blade::if('admin', function (User $user = null) {
-            return ($user ?? auth()->user())->isAdmin();
-        });
-
-        Blade::if('paid', function (Subscription $subscription) {
-            return $subscription->paid_at != null;
-        });
-
-        Blade::if('subscriptionRoute', fn () => request()->route()->named('users.subscriptions.index'));
+//        Blade::if('admin', function (User $user = null) {
+//            return ($user ?? auth()->user())->isAdmin();
+//        });
+//
+//        Blade::if('paid', function (Subscription $subscription) {
+//            return $subscription->paid_at != null;
+//        });
+//
+//        Blade::if('subscriptionRoute', fn () => request()->route()->named('users.subscriptions.index'));
     }
 
-    public function registerResponseMacros()
+
+    /**
+     * Creates a Response macro for API json responses having a standard format;
+     */
+        public function registerApiResponseMacro(): void
     {
-        /**
-         * Creates a standard json structure for consistent api responses in a simple way.
-         * {"status": true, "title": "Some title", "message": "Successful", "data": [a, b, c]}
-         */
-        Response::macro('api', function ($response, $status = 200) {
-            $format = ['status' => ($status < 400), 'title' => '', 'message' => '', 'data' => []];
-
-            // For convenience, if $response is a string, we'll use it as the message...
-            if (! is_array($response)) $response = ['message' => $response];
-
-            return response(array_merge($format, $response), $status);
+        Response::macro('api', function (string $message = '', $data = [], $status = 200, array $headers = []) {
+            return response()->json(['message' => $message, 'data' => $data], $status, $headers);
         });
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\UserSubscriptionController;
 use App\Http\Controllers\Api\SubscriptionController;
+use App\Http\Controllers\PayAzaPaymentController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,13 +19,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('guest')->group(function () {
-    Route::post('/login',       [AuthController::class, 'login'])->name('login');
-    Route::post('/register',    [RegisterController::class, 'store'])->name('register.store');
-});
+//Route::middleware('guest')->group(function () {
+//    Route::post('/login',       [AuthController::class, 'login'])->name('login');
+//    Route::post('/register',    [RegisterController::class, 'store'])->name('register.store');
+//});
 
-Route::middleware('auth')->group(function () {
-    Route::resource('users', UserController::class)->only(['store', 'update', 'destroy']);
-    Route::resource('users.subscriptions', UserSubscriptionController::class)->only('store');
-    Route::post('/subscriptions/{subscription}', [SubscriptionController::class, 'show'])->name('subscriptions.show');
-});
+//Route::middleware('auth')->group(function () {
+//    Route::resource('users', UserController::class)->only(['store', 'update', 'destroy']);
+//    Route::resource('users.subscriptions', UserSubscriptionController::class)->only('store');
+//    Route::post('/subscriptions/{subscription}', [SubscriptionController::class, 'show'])->name('subscriptions.show');
+//});
+
+Route::get('/virtual-accounts/{id}',     [PayAzaPaymentController::class, 'getVirtualAccountDetails']);
+Route::post('/virtual-accounts',         [PayAzaPaymentController::class, 'createVirtualAccount']);
+Route::post('/reserved-accounts',     [PayAzaPaymentController::class, 'createReservedAccount']);
+Route::get('/reserved-accounts-history/{id}',         [PayAzaPaymentController::class, 'getReservedTransactionHistory']);
+
